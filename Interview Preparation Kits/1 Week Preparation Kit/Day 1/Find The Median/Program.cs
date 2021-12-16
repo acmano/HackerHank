f3118955e41f5dd2f87587b6897b08c06a719405
Antonio Carlos Mano <antonio.mano@gmail.com>
2021-12-16 15:02:11 -0300

    Find The Median

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1/Find The Median/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Find_The_Median
+{
+  internal class Program
+  {
+    private static int FindMedian
+    (
+      List<int> arr
+    )
+    {
+      arr.Sort();
+      int x = (arr.Count - 1) / 2;
+      return arr[x];
+    }
+
+    private static void Main()
+    {
+      TextWriter textWriter = new StreamWriter
+      (
+        Environment.GetEnvironmentVariable
+        (
+          "OUTPUT_PATH"
+        )
+        ?? string.Empty
+      , true
+      );
+      int n = Convert.ToInt32
+      (
+        Console.ReadLine()
+              ?.Trim()
+      );
+      List<int> arr = Console.ReadLine()
+                               ?.TrimEnd()
+                                .Split
+                                 (
+                                   ' '
+                                 )
+                                .ToList()
+                                .Select
+                                 (
+                                   arrTemp => Convert.ToInt32
+                                   (
+                                     arrTemp
+                                   )
+                                 )
+                                .ToList();
+      int result = FindMedian
+      (
+        arr
+      );
+      textWriter.WriteLine
+      (
+        result
+      );
+      textWriter.Flush();
+    }
+  }
+}
