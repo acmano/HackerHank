5fec6ef386c9c44c4b1164e77a0b5822aef18f11
Antonio Carlos Mano <antonio.mano@gmail.com>
2021-12-16 10:19:21 -0300

    Time Conversion

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1/Mini-Max Sum/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static System.Console;
+
+namespace Mini_Max_Sum
+{
+  internal static class Program
+  {
+    private static void MiniMaxSum
+    (
+      List<long> arr
+    )
+    {
+      arr.Sort();
+      long sumMinor = 0;
+      long sumMax = 0;
+      for (int i = 0
+            ; i < 5
+            ; i++)
+      {
+        if (arr[i] < 1
+             || arr[i] > 1000000000)
+        {
+          arr[i] = 0;
+        }
+
+        if (i < 4)
+        {
+          sumMinor += arr[i];
+        }
+
+        if (i > 0)
+        {
+          sumMax += arr[i];
+        }
+      }
+
+      WriteLine
+      (
+        "{0} {1}"
+      , sumMinor
+      , sumMax
+      );
+    }
+
+    public static void Main
+    (
+      string[] args
+    )
+    {
+      List<long> arr = ReadLine()
+                        ?.TrimEnd()
+                         .Split
+                          (
+                            ' '
+                          )
+                         .ToList()
+                         .Select
+                          (
+                            arrTemp => Convert.ToInt64
+                            (
+                              arrTemp
+                            )
+                          )
+                         .ToList();
+
+      MiniMaxSum
+      (
+        arr
+      );
+    }
+  }
+}
---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1/Time Conversion/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+using static System.Environment;
+
+namespace Time_Conversion
+{
+  internal class Program
+  {
+    private static string TimeConversion
+    (
+      string s
+    )
+    {
+      int hour = Convert.ToInt32
+      (
+        s.Substring
+        (
+          0
+        , 2
+        )
+      );
+      if (s.Substring
+           (
+             8
+           , 2
+           )
+           == "PM")
+      {
+        if (hour >= 1
+             && hour <= 11)
+        {
+          hour += 12;
+        }
+      }
+      else
+      {
+        if (hour == 12)
+        {
+          hour = 0;
+        }
+      }
+
+      s = hour.ToString
+          (
+            "00"
+          )
+          + s.Substring
+          (
+            2
+          , 6
+          );
+
+      return s;
+    }
+
+    private static void Main
+    (
+      string[] args
+    )
+    {
+      TextWriter textWriter = new StreamWriter
+      (
+        GetEnvironmentVariable
+        (
+          "OUTPUT_PATH"
+        )
+        ?? string.Empty
+      , true
+      );
+
+      string s = Console.ReadLine();
+
+      string result = TimeConversion
+      (
+        s
+      );
+
+      textWriter.WriteLine
+      (
+        result
+      );
+
+      textWriter.Flush();
+      textWriter.Close();
+    }
+  }
+}
